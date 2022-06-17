b0d1c0ae2f94193faf4f3cad132b68cdf3f054fb
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2022-06-17 16:12:09 +0900

    GUI版追加

---
+++ GUI_Ver/jpg16_9resizeGUI.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import math
+import pathlib
+import PySimpleGUI as sg
+result = sg.popup_get_folder("フォルダを選択してください")
+if result is None:
+   result =""
+exclulists,jpglists = [],[]
+exclupath = pathlib.Path(result).glob('16_9_*.jpg')
+for e in exclupath:
+    exclulists.append(e.name)
+jpgpath = pathlib.Path(result).glob('*.jpg')
+for j in jpgpath:
+    jpglists.append(j.name)
+for je in  range(len(exclulists)):
+    jpglists.remove(exclulists[je])
+
+for jr in range(len(jpglists)):
+ img = Image.open(result+"/"+jpglists[jr])
+ width, height = img.size
+ if width/height <= 16/9:
+  re_height = height/1080
+  if re_height > 1 :
+     re_width = math.ceil(width/re_height)
+  elif re_height <= 1 :
+     re_height = 1080/height
+     re_width = math.ceil(width*re_height)
+  re_img = img.resize((re_width, 1080))
+  width, height = re_img.size
+  y_coordi = int((1920-width)/2)
+  back_img = Image.new('RGB',(1920,1080),color=(0,0,0))
+  back_img.paste(re_img, (y_coordi, 0)) 
+  
+ if width/height > 16/9:
+  re_width = width/1920
+  if re_width > 1 :
+     re_height = math.ceil(height/re_width)
+  elif re_width <= 1 :
+     re_width = 1920/width
+     re_height = math.ceil(height*re_width)
+  re_img = img.resize((1920, re_height))
+  width, height = re_img.size
+  x_coordi = int((1080-height)/2)
+  back_img = Image.new('RGB',(1920,1080),color=(0,0,0))
+  back_img.paste(re_img, (0, x_coordi)) 
+
+ back_img.save(result+"/16_9_"+jpglists[jr], quality=95)
+if result !="":
+ sg.popup('変換が完了しました')
---
+++ GUI_Ver/png4_3resizeGUI.py
@@ -0,0 +1,58 @@
+from PIL import Image, ImageDraw
+import math
+import pathlib
+import PySimpleGUI as sg
+result = sg.popup_get_folder("フォルダを選択してください")
+if result is None:
+   result =""
+exclulists,pnglists = [],[]
+exclupath = pathlib.Path(result).glob('4_3_*.png')
+for e in exclupath:
+    exclulists.append(e.name)
+pngpath = pathlib.Path(result).glob('*.png')
+for p in pngpath:
+    pnglists.append(p.name)
+for pe in  range(len(exclulists)):
+    pnglists.remove(exclulists[pe])
+print (result)
+#print(exclulists)
+print(pnglists)
+
+for pr in range(len(pnglists)):
+ img = Image.open(result+pnglists[pr])
+
+ width, height = img.size
+ print(width, height)
+ if width/height <= 4/3:
+  re_height = height/1080
+  if re_height > 1 :
+     re_width = math.ceil(width/re_height)
+  elif re_height <= 1 :
+     re_height = 1080/height
+     re_width = math.ceil(width*re_height)
+  re_img = img.resize((re_width, 1080))
+  width, height = re_img.size
+  y_coordi = int((1440-width)/2)
+  back_imga = Image.new('RGBA',(width, height),color=(0,0,0,0))
+  back_img = Image.new('RGBA',(1440,1080),color=(0,0,0,255))
+  back_img.paste(back_imga, (y_coordi, 0)) 
+  back_img.paste(re_img, (y_coordi, 0)) 
+
+ if width/height > 4/3:
+  re_width = width/1440
+  if re_width > 1 :
+     re_height = math.ceil(height/re_width)
+  elif re_width <= 1 :
+     re_width = 1440/width
+     re_height = math.ceil(height*re_width)
+  re_img = img.resize((1440, re_height))
+  width, height = re_img.size
+  x_coordi = int((1080-height)/2)
+  back_imga = Image.new('RGBA',(width, height),color=(0,0,0,0))
+  back_img = Image.new('RGBA',(1440,1080),color=(0,0,0,255))
+  back_img.paste(back_imga, (0, x_coordi))   
+  back_img.paste(re_img, (0, x_coordi))   
+
+ back_img.save(result+"4_3_"+pnglists[pr], quality=95)
+if result !="":
+ sg.popup('変換が完了しました')
